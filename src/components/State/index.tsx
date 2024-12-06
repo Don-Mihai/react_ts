937da7f876b2812eedf59bd326f17b5b73570351
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2024-12-06 20:22:34 +0300

    users

--- src/components/State/index.tsx
+++ src/components/State/index.tsx
@@ -1,23 +1,34 @@
-const State = () => {
-    let counter = 0;
+import { useState } from 'react';
 
-  
+const State = () => {
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(['пользователь 1', 'пользователь 2']);
 
     const increase = () => {
-        counter++;
-        console.log(counter);
+        setCounter(prev => prev + 1);
+        setCounter(prev => prev + 1);
+        setCounter(prev => prev + 1);
     };
 
     const decrease = () => {
-        counter--;
-        console.log(counter);
+        setCounter(counter - 1);
     };
+
+    const addUser = () => {
+        setUsers([...users, 'новый пользователь']);
+    };
+
+    console.log(users);
 
     return (
         <div>
             <button onClick={increase}>+</button>
             {counter}
             <button onClick={decrease}>-</button>
+            {users.map(user => (
+                <h2>{user}</h2>
+            ))}
+            <button onClick={addUser}>Добавить пользователя в тетрадь</button>
         </div>
     );
 };
